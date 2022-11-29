538816521e506605c90655d46d5ebf093991b3f3
proxy0001 <g.proxy0001@gmail.com>
2022-11-29 18:40:10 +0800

    rm useless

--- components/MultiRangeSlider.tsx
+++ components/MultiRangeSlider.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from 'react'
+import { useState } from 'react'
 import styles from '../styles/MultiRangeSlider.module.css'
 import { OnChange } from '../@types/app'
 
